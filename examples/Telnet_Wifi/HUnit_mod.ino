749a1e6f678c9b326e79a5c1b6a3f977ad320365
fredycpu <fred@microinfo.fr>
2021-04-30 22:13:40 +0200

    Update HUnit_mod.ino

--- examples/Telnet_Wifi/HUnit_mod.ino
+++ examples/Telnet_Wifi/HUnit_mod.ino
@@ -4,11 +4,11 @@
 #define SERIAL_SPEED    19200
 JunsunPSACANRemote* remote;
 
-Void setupHUnit() {
+void setupHUnit() {
   Serial.begin(SERIAL_SPEED);  
   remote = new JunsunPSACANRemote(Serial);
 }
 
-Void HUnit() {
+void HUnit() {
   
 }
