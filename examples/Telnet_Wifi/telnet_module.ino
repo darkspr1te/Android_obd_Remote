36b292889d13aadcaa92c42e1d34f07bd5d592ee
fredycpu <fred@microinfo.fr>
2021-04-30 11:51:02 +0200

    Update telnet_module.ino

--- examples/Telnet_Wifi/telnet_module.ino
+++ examples/Telnet_Wifi/telnet_module.ino
@@ -2,7 +2,6 @@
 #include "ESPTelnet.h"          // https://github.com/LennartHennigs/ESPTelnet
 
 ESPTelnet telnet;
-IPAddress ip;
 
 void telent_conf() {      
   telnet.onConnect(onTelnetConnect);
@@ -56,34 +55,7 @@ void onTelnetReconnect(String ip) {
 void onTelnetConnectionAttempt(String ip) {
 }
 
-bool isConnected() {
-  return (WiFi.status() == WL_CONNECTED);
-}
-
-bool connectToWiFi(const char* ssid, const char* password, int max_tries = 20, int pause = 500) {
-  int i = 0;
-  WiFi.mode(WIFI_STA);
-  #if defined(ARDUINO_ARCH_ESP8266)
-    WiFi.forceSleepWake();
-    delay(200);
-  #endif
-  WiFi.begin(ssid, password);
-  do {
-    delay(pause);
-  } while (!isConnected() || i++ < max_tries);
-  WiFi.setAutoReconnect(true);
-  WiFi.persistent(true);
-  return isConnected();
-}
-
 void setupTelnet() {
-  #ifndef websrv_mod 
-    connectToWiFi(WIFI_SSID, WIFI_PASSWORD);
-      if (isConnected()) {
-      ip = WiFi.localIP();
-    }
-  #endif
-    
   telent_conf();
   if (telnet.begin()) {
   }
