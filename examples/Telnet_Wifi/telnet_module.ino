c7bb7810ca3d96f6c3e5bd3a56d34b3d80f2a109
fredycpu <fred@microinfo.fr>
2021-04-30 12:42:44 +0200

    Update telnet_module.ino

--- examples/Telnet_Wifi/telnet_module.ino
+++ examples/Telnet_Wifi/telnet_module.ino
@@ -5,37 +5,47 @@ ESPTelnet telnet;
 
 void telent_conf() {      
   telnet.onConnect(onTelnetConnect);
-  telnet.onConnectionAttempt(onTelnetConnectionAttempt);
-  telnet.onReconnect(onTelnetReconnect);
   telnet.onDisconnect(onTelnetDisconnect);
 
   telnet.onInputReceived([](String str) {
-    if (str == "b:source") {
-      remote->SendButtonCode(Source);
-      telnet.println("CON:>>HU Source");
-    }
-    if (str == "b:vol+") {
-      remote->SendButtonCode(VolumeUp);
-      telnet.println("CON:>>HU Vol+");
-    }
-    if (str == "b:vol-") {
-      remote->SendButtonCode(VolumeDown);
-      telnet.println("CON:>>HU Vol-");
-    }
+    #if defined(HUnit_mod)
+      if (str == "b:source") {
+        remote->SendButtonCode(Source);
+        telnet.println("CON:>>HU Source");
+      }
+      if (str == "b:vol+") {
+        remote->SendButtonCode(VolumeUp);
+        telnet.println("CON:>>HU Vol+");
+      }
+      if (str == "b:vol-") {
+        remote->SendButtonCode(VolumeDown);
+        telnet.println("CON:>>HU Vol-");
+      }
+      if (str == "?b:") { //help 
+        telnet.println("Possible values :\n");
+        telnet.println("source vol+ vol-");
+      }
+    #endif
     if (str == "?") { //help 
-      telnet.println("Commands :\n b:<value> Send <value> button to Head Unit use ?b: to see available values");
-      telnet.println("r:<value> Send raw <value> Head Unit (start and chekcsum will be added) use ?r: for help");
-      telnet.println("t:<number> <value> Send trip data to Head Unit use ?t: for help");
-      telnet.println("a:<values> Send A/C data to Head Unit use ?a: for help");  
-      telnet.println("hm:<value> filter received data by HU by function ");
-      telnet.println("Hm:<value> filter emmited data by HU by function ");
-      telnet.println("cm:<value> filter received data by CAN by function ");
-      telnet.println("Cm:<value> filter emmited data by CAN by function ");
+      telnet.println("Commands :");
+      #if defined(HUnit_mod)
+        telnet.println("b:<value> Send <value> button to Head Unit use ?b: to see available values");
+        telnet.println("r:<value> Send raw <value> to Head Unit (start and chekcsum will be added) use ?r: for help");
+        telnet.println("t:<number> <value> Send trip data to Head Unit use ?t: for help");
+        telnet.println("a:<values> Send A/C data to Head Unit use ?a: for help");  
+        telnet.println("hm:<value> filter received data by HU by function ");
+        telnet.println("Hm:<value> filter emmited data by HU by function ");
+      #endif                
+      #if defined(canbus1_mod)                       
+        telnet.println("cm:<value> filter received data by CAN by function ");
+        telnet.println("Cm:<value> filter emmited data by CAN by function ");
+      #endif
+      #if defined(canbus2_mod)                       
+        telnet.println("c2m:<value> filter received data by CAN2 by function ");
+        telnet.println("C2m:<value> filter emmited data by CAN2 by function ");
+      #endif
     }
-    if (str == "?b:") { //help 
-      telnet.println("Possible values :\n");
-      telnet.println("source vol+ vol-");
-    }
+
   });
 }
     
@@ -47,12 +57,6 @@ void onTelnetConnect(String ip) {
 }
 
 void onTelnetDisconnect(String ip) {
-}
-
-void onTelnetReconnect(String ip) {
-}
-
-void onTelnetConnectionAttempt(String ip) {
 }
 
 void setupTelnet() {
