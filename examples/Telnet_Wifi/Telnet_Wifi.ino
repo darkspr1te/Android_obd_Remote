55efe26d4717cd63ab9f4b931bb17251eeed0463
fredycpu <fred@microinfo.fr>
2021-05-01 12:27:31 +0200

    Update Telnet_Wifi.ino

--- examples/Telnet_Wifi/Telnet_Wifi.ino
+++ examples/Telnet_Wifi/Telnet_Wifi.ino
@@ -13,23 +13,18 @@
 
 #define FILESYSTEM SPIFFS
 
-#define IO_HUenable       //Poweron
-#define IO_HUrear         //rear gear signal
-#define IO_HUbrake        //handbrake signal
-#define IO_HUill          //Illimation signal
-
 struct Car {
-    uint16_t I_speed, I_rpm;
-    bool ign,ill,rear,brake;
-    byte AC_L,AC_R,AC_FAN;
-    bool AC_DOWN,AC_UP,AC_FRONT,AC_Recycle,AC_WINDSH,AC_AUTO,AC_COMP,AC_OFF;
-    bool H_UPD_AC,H_UPD_I,H_UPD_IO;
+    uint16_t I_speed, I_rpm; // odometer info
+    bool ign,ill,rear,brake; // I/O status
+    byte AC_L,AC_R,AC_FAN; // A/C values
+    bool AC_DOWN,AC_UP,AC_FRONT,AC_Recycle,AC_WINDSH,AC_AUTO,AC_COMP,AC_OFF; 
+    bool DOOR_FL,DOOR_FR,DOOR_RL,DOOR_RR,DOOR_TR; //door status
+    bool H_UPD_AC,H_UPD_I,H_UPD_IO,H_UPD_DOOR; //update flag
 };
 
 
 
 void setup() {
-    setup_IO();
   #if defined(HUnit_mod) 
     setupHUnit();
   #endif
@@ -58,12 +53,6 @@ void loop() {
   #if defined(WIFI_mod) 
     WIFI_loop();
   #endif
-}
-void setup_IO() {
-  pinMode(IO_HUenable,OUTPUT);
-  pinMode(IO_HUrear,OUTPUT);
-  pinMode(IO_HUbrake,OUTPUT);
-  pinMode(IO_HUill,OUTPUT);
 }
 
 void errorMsg(String error, bool restart = true) {
