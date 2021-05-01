8b6f0b66b5b7b21cc1313a40fc74db14545bd1e6
fredycpu <fred@microinfo.fr>
2021-05-01 12:27:03 +0200

    Update HUnit_mod.ino

--- examples/Telnet_Wifi/HUnit_mod.ino
+++ examples/Telnet_Wifi/HUnit_mod.ino
@@ -1,14 +1,28 @@
 #define HUnit_mod 
 #include <JunsunPSACANRemote.h>
 
+#define IO_HUenable       //Poweron
+#define IO_HUrear         //rear gear signal
+#define IO_HUbrake        //handbrake signal
+#define IO_HUill          //Illimation signal
+
 #define SERIAL_SPEED    19200
 JunsunPSACANRemote* remote;
 
 void setupHUnit() {
+  setup_IO();
   Serial.begin(SERIAL_SPEED);  
   remote = new JunsunPSACANRemote(Serial);
 }
 
 void HUnit() {
   
+}
+
+
+void setup_IO() {
+  pinMode(IO_HUenable,OUTPUT);
+  pinMode(IO_HUrear,OUTPUT);
+  pinMode(IO_HUbrake,OUTPUT);
+  pinMode(IO_HUill,OUTPUT);
 }
