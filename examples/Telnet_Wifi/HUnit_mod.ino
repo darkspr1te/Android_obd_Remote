e63fb523c6a214423b45ba0df318a27da19c927b
fredycpu <fred@microinfo.fr>
2021-05-02 10:30:18 +0200

    Update HUnit_mod.ino

--- examples/Telnet_Wifi/HUnit_mod.ino
+++ examples/Telnet_Wifi/HUnit_mod.ino
@@ -1,10 +1,10 @@
 #define HUnit_mod 
 #include <JunsunPSACANRemote.h>
 
-#define IO_HUenable       //Poweron
-#define IO_HUrear         //rear gear signal
+#define IO_HUenable       //Car ignition signal
+#define IO_HUrear         //reverse gear signal
 #define IO_HUbrake        //handbrake signal
-#define IO_HUill          //Illimation signal
+#define IO_HUill          //Illumimation signal
 
 #define SERIAL_SPEED    19200
 JunsunPSACANRemote* remote;
@@ -16,7 +16,7 @@ void setupHUnit() {
 }
 
 void HUnit() {
-  
+  if (Car.H_UPD_IO) update_IO();
 }
 
 
@@ -25,4 +25,13 @@ void setup_IO() {
   pinMode(IO_HUrear,OUTPUT);
   pinMode(IO_HUbrake,OUTPUT);
   pinMode(IO_HUill,OUTPUT);
+  update_IO();
+}
+
+void update_IO() {
+  digitalWrite(IO_HUenable, ign); // put !ign if nedds to reversed
+  digitalWrite(IO_HUrear, rear); // put !rear if nedds to reversed
+  digitalWrite(IO_HUbrake, brake); // put !brake if nedds to reversed
+  digitalWrite(IO_HUill, ill);// put !ill if nedds to reversed
+  Car.H_UPD_IO=false; //updated
 }
